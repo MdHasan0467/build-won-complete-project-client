cb66399b1514cad459472f4097928a5266b070e1
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-23 22:49:29 +0600

    created routes and customized

---
+++ src/components/Pages/DynamicProductCategory/DynamicProductCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DynamicProductCategory = () => {
+    return (
+			<div>
+				<h1>DynamicProductCategory page</h1>
+			</div>
+		);
+};
+
+export default DynamicProductCategory;
