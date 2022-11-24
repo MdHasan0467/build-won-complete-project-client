6c575fd426c3cd8c2f8d3f8a6619a671e5db773d
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-24 23:10:28 +0600

    feedback section added

--- src/components/Pages/Home/ExtraSection/Feedback/Feedback.js
+++ src/components/Pages/Home/ExtraSection/Feedback/Feedback.js
@@ -5,7 +5,39 @@ const Feedback = () => {
     	const {register,handleSubmit,formState: { errors },} = useForm();
     return (
 			<div className='flex justify-center bg-slate-300'>
-				
+				<form>
+					<div className='form-control w-[300px]'>
+						<label className='label'>
+							<span className='label-text'>Your Email</span>
+						</label>
+						<input
+							type='email'
+							{...register('email', {
+								required: true,
+							})}
+							className='input input-bordered w-full'
+						/>
+						{errors.email && (
+							<p className='text-red-500'>{errors.email.message}</p>
+						)}
+					</div>
+					<div className='form-control w-[300px]'>
+						<label className='label'>
+							<span className='label-text'>Your Feedback</span>
+						</label>
+						<input
+							type='text'
+							placeholder='Give here your experience'
+							{...register('text', {
+								required: true,
+							})}
+							className='input input-bordered w-full h-[100px]'
+						/>
+						{errors.text && (
+							<p className='text-red-500'>{errors.text.message}</p>
+						)}
+					</div>
+				</form>
 			</div>
 		);
 };
